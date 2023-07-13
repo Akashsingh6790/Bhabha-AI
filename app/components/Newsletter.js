c8703cd87bed495f13447a3ab8c6f4ceb588cd44
Ghanshyam-03 <gsrajput2002@gmail.com>
2023-07-13 10:25:08 +0530

    newsletter updated

--- app/components/Newsletter.js
+++ app/components/Newsletter.js
@@ -3,27 +3,25 @@ import React from 'react'
 function Newsletter() {
     return (
         
-       <div className="bg-white w-full"> <div className="text-5xl text-purple-700  text-center">Follow our journey</div>
-       <div className=" bg-white text-xl text-purple-700 bg-black text-center mt-8">Sign Up with your email address to receive news and updates.</div>
+       <div className="bg-white w-full">
+        <div className="text-5xl text-purple-700 font-bold text-center pt-9">Follow our journey</div>
+       <div className=" bg-white text-xl text-purple-700 text-center mt-8">Sign Up with your email address to receive news and updates.</div>
 
-     <div className='bg-white h-20'> <form className="flex justify-center mt-5">
-       
+     <div className='bg-white h-20'>
+      <form className="flex justify-center mt-5">
         <input
         type='email'
-        placeholder='Email Address'
-        style={{ width: '150px', height: '50px', border: '1px solid black', margin: '14px'}}
-
+        placeholder='      Email Address        '
+        style={{ width: '225px', height: '50px', border: '1px solid black', margin: '14px'}}
+        className='text-black px-4'
         />
-       <button className="bg-purple-700 hover:bg-purple-400 text-white font-bold py-1 px-2 rounded mt-4 mb-4">
-  Sign Up
+       <button className="bg-purple-700 hover:bg-purple-400 text-white font-bold py-3 px-5 rounded mt-4 mb-4">
+  Suscribe
 </button>
-       
-       
-       
-       </form>
+</form>
 </div>
 
-<div className='bg-white text-purple-700 text-xl text-center mt-8 h-full'>We respect your privacy. Please read our Terms and Conditions & Privacy pages to learn more. </div>
+<div className='bg-white text-purple-700 text-xl text-center mt-8 h-full pb-9'>We respect your privacy. Please read our Terms and Conditions & Privacy pages to learn more. </div>
 
 
 
